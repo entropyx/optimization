d8c685e5904c26d567cae89b07ff21261115ef25
gibran <gibran@entropy.tech>
2016-12-22 17:38:30 -0600

    Reflection function done.

--- neldermead.go
+++ neldermead.go
@@ -1,1 +1,9 @@
 package optimization
+
+func reflection(x []float64, c []float64, alpha float64) (out []float64) {
+	l := len(x)
+	for i := 0; i < l; i++ {
+		out = append(out, c[i]+alpha*(c[i]-x[i]))
+	}
+	return
+}
--- neldermead_test.go
+++ neldermead_test.go
@@ -1,1 +1,20 @@
 package optimization
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestNelderMead(t *testing.T) {
+
+	Convey("Given the vector [1 2 3] and the centroid [0 0 0]", t, func() {
+		x := []float64{1, 2, 3}
+		c := []float64{0, 0, 0}
+		Convey("The reflection of x is [-1 -2 -3]", func() {
+			alpha := 1.00
+			out := reflection(x, c, alpha)
+			So(out, ShouldResemble, []float64{-1, -2, -3})
+		})
+	})
+}
