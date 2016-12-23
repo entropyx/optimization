ed68f982f8bc003dadfa9f129193de7964b5135f
gibran <gibran@entropy.tech>
2016-12-22 18:11:29 -0600

    Nelder-Mead start

--- neldermead.go
+++ neldermead.go
@@ -1,5 +1,9 @@
 package optimization
 
+type function interface {
+	f() []float64
+}
+
 func reflection(x []float64, c []float64, alpha float64) (out []float64) {
 	l := len(x)
 	for i := 0; i < l; i++ {
@@ -30,4 +34,8 @@ func shrink(x []float64, y []float64, delta float64) (out []float64) {
 		out = append(out, x[i]+delta*(y[i]-x[i]))
 	}
 	return
+}
+
+func neldermead(variable []float64, fn function) {
+
 }
