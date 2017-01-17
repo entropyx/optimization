16a09730c5ba4bd3b65bcef812151a3e8f974f92
gibran <gibran@entropy.tech>
2017-01-17 17:25:35 -0600

    Testing SGD.

--- stochasticgradientdescent_test.go
+++ stochasticgradientdescent_test.go
@@ -9,6 +9,8 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
+var lambda2 = 0.0001
+
 //H hypothesis
 func H2(Xi, theta []float64) float64 {
 	prod := ma.VectorProduct(Xi, theta)
@@ -22,7 +24,7 @@ func Grad2(theta []float64) (grad []float64) {
 	yi := y[sample]
 	error := H2(Xi, theta) - yi
 	for j := 0; j < len(Xi); j++ {
-		grad = append(grad, Xi[j]*error)
+		grad = append(grad, Xi[j]*error-lambda2*theta[j])
 	}
 	return
 }
@@ -32,10 +34,10 @@ func TestStochasticGradientDecentFunction(t *testing.T) {
 		theta := []float64{0, 0, 0}
 
 		Convey("... must converge to [-25.16133355416168 0.20623171363284806 0.20147159995083574]", func() {
-			minimum := StochasticGradientDecent(Grad, theta, 1.0, 100000)
+			minimum := StochasticGradientDecent(Grad2, theta, 0.1, 100000)
 			minimum = ma.RescaleCoef(minimum, mu, sigma)
 			fmt.Printf("Stochastic Gradient Decent Minimum: %v \n", minimum)
-			So(minimum, ShouldResemble, []float64{-25.0174711172546, 0.20507347261082987, 0.20030381297897873})
+			So(minimum, ShouldResemble, []float64{-23.663158419526443, 0.19485272382138286, 0.18867193554561834})
 		})
 	})
 }
